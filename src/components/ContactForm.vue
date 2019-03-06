be462be23e235d72fbcb8bbc43f75a7565397d41
Ashkan Arvaneh <ashkan.arvaneh@gmail.com>
2019-03-06 20:56:33 +0000

    Tech-test Add basic contact form component and its relevant data

---
+++ src/components/ContactForm.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="contact-form">
+    <div class="card card-body">
+      <h3 class="text-center mb-4">Request a callback</h3>
+      <form>
+        <div class="form-group" v-for="form in contactFormData" :key="form.id">
+          <label for="fullName">{{ form.name }}</label>
+          <input
+            class="form-control"
+            :type="form.type"
+            :name="form.name"
+            :placeholder="form.name"
+            :id="form.id"
+            :aria-describedby="form.id"
+          >
+        </div>
+        <input class="btn btn-lg btn-primary" value="Call Me" type="submit">
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ContactForm"
+};
+</script>
+
+<style scoped lang="scss">
+</style>
